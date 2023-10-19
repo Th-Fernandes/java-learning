f12a699cd997c680b6a86b76ad99293683b0fd86
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-10-19 17:01:19 -0300

    refactor: user class on different file

---
+++ src/User.java
@@ -0,0 +1,19 @@
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public String getFullName() {
+        return String.join(" ", firstName, lastName);
+    }
+    public void setName(String firstName, String lastName) {
+        throwEmptyInputsError(firstName, lastName);
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    private void throwEmptyInputsError(String firstName, String lastName) {
+        boolean hasEmptyInputs = firstName.length() <= 0 || lastName.length() <= 0;
+        if (hasEmptyInputs) throw new IllegalArgumentException("Inputs must not be empty");
+    }
+}
