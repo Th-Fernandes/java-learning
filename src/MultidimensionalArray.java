059035773c8b1706f7b1a56d9618ecee1048298e
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-10-23 11:02:46 -0300

    fix: print correct row index instead of hash

--- src/MultidimensionalArray.java
+++ src/MultidimensionalArray.java
@@ -2,16 +2,15 @@ import java.util.ArrayList;
 
 public class MultidimensionalArray {
     final private ArrayList<int[]> gameBoard = new ArrayList<int[]>();
-    private int[] t = new int[5];
-
 
     public void setRow(int[] row) {
         gameBoard.add(row);
     }
 
     public void getGameBoard() {
-        for(int[] row : gameBoard) {
-            System.out.println("row number: " + row);
+        for(int i = 0; i < gameBoard.size(); i++) {
+            System.out.println("row " + i);
+            int[] row = gameBoard.get(i);
 
             for(int item : row)
                 System.out.print(item + " ");
